a8bb55848786799d194cd760a9c6307ae431bcb9
JoowonKim <joowon.kim@coxwave.com>
2021-10-21 17:30:01 +0900

    add

---
+++ tests/test_func1.py
@@ -0,0 +1,17 @@
+
+
+
+def test_add():
+    assert 1+2 == 3
+
+
+
+
+def test_add2():
+    assert 1+2 == 4
+
+
+
+
+def test_add3():
+    assert 1+2 == 5
---
+++ tests/test_func2.py
@@ -0,0 +1,6 @@
+
+def test2_add():
+    print(123)
+
+def test2_sample():
+    raise NotImplementedError()
